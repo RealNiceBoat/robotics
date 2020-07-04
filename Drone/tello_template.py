b8b837b4aae0729cf1da141324738ae4c63527db
Liu Haohui <liuhh02@gmail.com>
2020-07-04 23:18:09 +0800

    make image naming unique

--- Drone/tello_template.py
+++ Drone/tello_template.py
@@ -2,9 +2,7 @@ from Tello_api import Tello
 import cv2
 import time
 
-
-IMG_PATH = "/data/maze.jpg" # name of image captured by tello
-NEW_IMG_PATH = "/data/maze_processed.jpg" # for scoring
+i=0
 
 # 1: Tello move and capture image. Manually adjust Tello with keyboard. Press spacebar to capture and save image, press enter to retrieve Tello
 tello = Tello()
@@ -26,10 +24,12 @@ while True:
 	cv2.imshow('Tello video', cv2.flip(tello.frame,0))
 
 	k = cv2.waitKey(16) & 0xFF
-	if k == 32: # spacebar to capture image 
+	if k == 32: # spacebar to capture image
+		IMG_PATH = f"./image{i}.jpg" # name of image captured by tello
 		cv2.imwrite(IMG_PATH, cv2.flip(tello.frame,0))
 		print(f'Saved as {IMG_PATH}!')
-		cv2.imshow('Saved Image', cv2.flip(tello.frame,0)) 
+		cv2.imshow('Saved Image', cv2.flip(tello.frame,0))
+		i+=1
 
 	elif k == 13: # enter to retrieve drone, land and continue
 		padreply = tello._sendcommand('jump -100 0 100 60 0 m2 m1') # x y z speed yaw pad_ids
